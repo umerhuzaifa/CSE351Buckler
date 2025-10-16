bb5ac37b47d395f1bf6bf90c666665bcdd8332cc
umerhuzaifa <umerhuzaifa@gmail.com>
2025-10-16 15:34:57 -0700

    added more in the code

--- software/apps/interrupts/main.c
+++ software/apps/interrupts/main.c
@@ -7,16 +7,16 @@
 #include "buckler.h"
 
 void SWI1_EGU1_IRQHandler(void) {
-   
+   NRF_EGU1->EVENTS_TRIGGERED[0]=0;
+   // rest of the code follows here
 }
 
 // Step B: Write an IRQ Handler/ISR for the Interrupt
 void GPIOTE_IRQHandler(void) {
-   
+   // Clear 
 }
 
 int main(void) {
-  // Initialize LED and BUTTON pins on the Buckler board
   
 
     
@@ -26,10 +26,18 @@ int main(void) {
   NRF_LOG_DEFAULT_BACKENDS_INIT();
   printf("Log initialized!\n");
  
-  // Step A: Configuration 
+  // Initialize LED and BUTTON pins on the Buckler board
+  //............
+
+
+  // Steps for Setting up Interrupt
   // 1. configure the interrupt
-  // 2. enable interrupt
-  // 3. enable IRQ on NVIC 
+
+
+  // 2. enable interrupt -- NRF_GPIOTE->...
+
+
+  // 3. enable IRQ on NVIC  -- NVIC_EnableIRQ(...)
 
 
     while (1) {
